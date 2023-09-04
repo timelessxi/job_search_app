2e7ccf3b3c40222464c964cd4f95e488903f945d
Jamie Fury <jamiefury2@gmail.com>
2023-09-04 17:53:06 -0500

    Welcome screen UI

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,28 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"; // Make sure this import is correct
+import { useCallback, useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootview = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootview} />;
 };
 
 export default Layout;
