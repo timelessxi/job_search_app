20175a0942c027853f3dcba9656cf51e65714c00
Jamie Fury <jamiefury2@gmail.com>
2023-09-04 15:35:53 -0500

    Structure Setup & Styling

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 const Home = () => {
   return (
