6292de8808f2ea3e1369f2b6b961ff924da83182
Jamie Fury <jamiefury2@gmail.com>
2023-09-04 16:21:24 -0500

    Setup index.js

--- app/index.js
+++ app/index.js
@@ -6,10 +6,26 @@ import { COLORS, icons, images, SIZES } from "../constants";
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />,
+          headerRight: () => <ScreenHeaderBtn iconUrl={icons.profile} dimension='60%' />,
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
