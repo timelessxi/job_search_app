1485f737d7403a68a581a40120b20b818c319283
Jamie Fury <jamiefury2@gmail.com>
2023-09-04 20:31:57 -0500

    Popular Jobs Logos check

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,5 +1,5 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import { checkImageurl } from "../../../../utils";
+import { checkImageURL } from "../../../../utils";
 import styles from "./popularjobcard.style";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
@@ -11,7 +11,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
           source={{
-            uri: checkImageurl(item.employer_logo)
+            uri: checkImageURL(item.employer_logo)
               ? item.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
